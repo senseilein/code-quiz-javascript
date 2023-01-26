2b1d01232d4926355898b49b0b28181b48583973
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-01-26 12:24:12 +0000

    Improve generateOptions() function so that list items(answer options) are displayed inside buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,22 +3,20 @@
 - review list number in generateOptions()
 
 */
-// - an array of user objects > to store user.initials and user.score at the end 
-// - an array of score 
-// - an array of initials 
+// - an array of user objects > to store user.initials and user.score at the end
+// - an array of score
+// - an array of initials
 // ----'
-
 
 // --------------- DATA ------------------------------------------
 let timer = 59;
-let currentScore 
-
+let currentScore;
 
 // --------------- Create elements that we will need -------------
 
 // For feedbackText after each question
 let feedbackText = document.createElement("p");
-let horizontalLine = document.createElement("hr"); 
+let horizontalLine = document.createElement("hr");
 let list = document.createElement("ul");
 
 // --------------- Select Elements -------------------------------
@@ -33,64 +31,60 @@ let questionsDiv = document.getElementById("questions");
 let questionTitle = document.getElementById("question-title");
 let answerDiv = document.getElementById("choices");
 
-
-
 // --------------- Add content to Elements -----------------------
 feedbackText = "";
 
-
 // --------------- Append Elements -------------------------------
-
-
 
 // --------------- FUNCTIONS -------------------------------------
 
 // Transition from start-screen to screen with questions
 const displayQuestionScreen = () => {
-    startScreen.classList.add("hide");
-    questionsDiv.classList.remove("hide");
-}
+  startScreen.classList.add("hide");
+  questionsDiv.classList.remove("hide");
+};
 
 // Implementation of the count down to be used in the setInterval()
 const createCountDown = () => {
-    if(timer > 0){
+  if (timer > 0) {
     time.textContent = timer;
     timer--;
-    } else {
-        time.textContent = 0;
-        clearInterval(createCountDown);
-    }
-}
+  } else {
+    time.textContent = 0;
+    clearInterval(createCountDown);
+  }
+};
 
 // Populate #questions div with current question and options
 // We will then loop through the questions array to capture each question object individually
 const generateQuestion = (question) => {
-    questionTitle.textContent = question.title;
-}
-const generateOptions = (question) => {
-    question.answers.forEach(option => {
-        let listItem = document.createElement("li")
-        let optionBtn = document.createElement("button");
-        optionBtn.innerText = `${question.answers.indexOf(option) + 1}. ${option}`;
-        listItem.append(optionBtn);
-        list.append(listItem);
-        answerDiv.append(list);
-    });
-}
+  questionTitle.textContent = question.title;
+};
 
+// Answers will be displayed as list items inside buttons
+const generateOptions = (question) => {
+  question.answers.forEach((option) => {
+    let listItem = document.createElement("li");
+    let optionBtn = document.createElement("button");
+    listItem.innerText = option;
+    optionBtn.append(listItem);
+    list.append(optionBtn);
+    answerDiv.append(list);
+  });
+};
 
 // Regroup functions needed for the quiz to start
 const startQuiz = () => {
-    const launchCountDown = setInterval(createCountDown, 1000);
-    displayQuestionScreen();
-}
+  const launchCountDown = setInterval(createCountDown, 1000);
+  displayQuestionScreen();
+};
 
 const populateQuiz = () => {
-        //test
-    generateQuestion(questions[0]);
-    generateOptions(questions[0])
-}
+  //test
+  generateQuestion(questions[0]);
+  generateOptions(questions[0]);
+};
 
 // --------------- MAIN ------------------------------------------
 startBtn.addEventListener("click", startQuiz);
-populateQuiz()
+populateQuiz();
