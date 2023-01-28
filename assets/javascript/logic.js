4c10570cc00916e785f548f479d74cb1f4ba2cdc
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-01-28 12:54:03 +0000

    Cleanup and add logic for iterating through questions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,55 +1,44 @@
-// TO-DO LIST
-/*
-- review list number in generateOptions()
-
-*/
-// - an array of user objects > to store user.initials and user.score at the end
-// - an array of score
-// - an array of initials
-// ----'
-
 // --------------- DATA ------------------------------------------
 let timer = 59;
 let currentScore = 0;
-let currentQuestion = {};
+let currentQuestion;
 
 // --------------- Create elements that we will need -------------
 
 // For feedbackText after each question
-let correctFeedbackText = document.createElement("p");
-let wrongFeedbackText = document.createElement("p");
-let horizontalLine = document.createElement("hr");
+const correctFeedbackText = document.createElement("p");
+const wrongFeedbackText = document.createElement("p");
+const horizontalLine = document.createElement("hr");
 
 // To display answer options
-let listOfOptions = document.createElement("ul");
+const listOfOptions = document.createElement("ul");
 
 // --------------- Select Elements -------------------------------
 
 // On #start-screen
-let startBtn = document.getElementById("start");
-let time = document.getElementById("time");
-let startScreen = document.getElementById("start-screen");
+const startBtn = document.getElementById("start");
+const time = document.getElementById("time");
+const startScreen = document.getElementById("start-screen");
 
 // On #questions screen
-let questionDiv = document.getElementById("questions");
-let questionTitle = document.getElementById("question-title");
-let answerDiv = document.getElementById("choices");
+const questionScreen = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const answerDiv = document.getElementById("choices");
 
 // On #end-screen
-let endScreen = document.getElementById("end-screen");
+const endScreen = document.getElementById("end-screen");
 
-// --------------- Add content to Elements -----------------------
-correctFeedbackText.innerText = "Correct!";
-wrongFeedbackText.innerText = "Wrong!";
+let questionIndex = 0;
 
 // --------------- Append Elements -------------------------------
 
 // --------------- FUNCTIONS -------------------------------------
 
-// Transition from start-screen to screen with questions
+/*------- START -------*/
+// Transition from start-screen to screen with questions (questionScreen)
 const displayQuestionScreen = () => {
   startScreen.classList.add("hide");
-  questionDiv.classList.remove("hide");
+  questionScreen.classList.remove("hide");
 };
 
 // Implementation of the count down to be used in the setInterval()
@@ -63,8 +52,17 @@ const createCountDown = () => {
   }
 };
 
+// Regroup functions needed for the quiz to start
+const startQuiz = () => {
+  const launchCountDown = setInterval(createCountDown, 1000);
+  displayQuestionScreen();
+  populateQuiz();
+};
+
+/*------- QUESTION -------*/
 // Populate #questions div with current question and options
 // We will then loop through the questions array to capture each question object individually
+
 const generateQuestion = (question) => {
   questionTitle.textContent = question.title;
 };
@@ -83,74 +81,65 @@ const generateOptions = (question) => {
   });
 };
 
-const generateFeedback = (feedbackText) => {
-  answerDiv.appendChild(horizontalLine);
-  answerDiv.appendChild(feedbackText);
+const clearFeedback = () => {
+  answerDiv.removeChild(horizontalLine);
+
+  // target already existing text div, created in the parent function and remove it
+  const feedbackText = document.getElementById("feedback-div");
+  answerDiv.removeChild(feedbackText);
 };
 
-// Not correct
-// const disableAnswersAfterFeedback = () => {
-//   // remove classes so that nothing happens if user clicks again
-//   // const listItemWithClass = document.querySelectorAll("li");
-//   listItemWithClass.classList.remove("wrong", "correct");
-// };
+const generateFeedback = (feedbackText) => {
+  answerDiv.appendChild(horizontalLine);
+
+  // create text div and append it to the container
+  const feedbackDiv = document.createElement("div");
+  feedbackDiv.setAttribute("id", "feedback-div");
+  feedbackDiv.innerText = feedbackText;
+  answerDiv.appendChild(feedbackDiv);
+  setTimeout(clearFeedback, 1000);
+};
+
+const handleAnswer = () => {
+  questionTitle.textContent = "";
+  questionIndex++;
+  listOfOptions.textContent = "";
+  populateQuiz();
+};
 
 // Event delegation
 const validateUserAnswer = () => {
-  questionDiv.addEventListener("click", function (event) {
+  questionScreen.addEventListener("click", function (event) {
     let target = event.target;
+
     if (target.className === "correct") {
-      console.log("yeah " + target.textContent);
-      generateFeedback(correctFeedbackText);
+      generateFeedback("Correct!");
       currentScore++;
-      console.log("current score " + currentScore);
+      handleAnswer();
       return true;
     } else if (target.className === "wrong") {
-      console.log(" NO wrong! " + target.textContent);
-      generateFeedback(wrongFeedbackText);
+      generateFeedback("Wrong!");
       timer -= 10;
+      handleAnswer();
+
       return false;
-    } else {
-      console.log("meh");
     }
   });
 };
 
-const clearQuestion = () => {
-  questionTitle.textContent = "";
-  answerDiv.removeChild(listOfOptions);
-};
-
-// Regroup functions needed for the quiz to start
-const startQuiz = () => {
-  const launchCountDown = setInterval(createCountDown, 1000);
-  displayQuestionScreen();
-};
-
 const populateQuiz = () => {
-  //test
-  generateQuestion(questions[0]);
-  generateOptions(questions[0]);
+  generateQuestion(questions[questionIndex]);
+  generateOptions(questions[questionIndex]);
 };
 
 const endQuiz = () => {
   startScreen.classList.add("hide");
-  questionDiv.classList.add("hide");
+  questionScreen.classList.add("hide");
   endScreen.classList.remove("hide");
 };
 
-// --------------- MAIN ------------------------------------------
+// --------------- EVENT LISTENERS ------------------------------------------
 
 startBtn.addEventListener("click", startQuiz);
 
-/* For each question, we will 
-- populate the quiz then clear it once user clicked the correct target 
-> do this as long as there are questions available
-
-*/
-populateQuiz();
-validateUserAnswer(); // and display feedback
-//disableAnswersAfterFeedback();
-// clearQuestion();
-
-// endQuiz();
+validateUserAnswer();
